52a15ee81e59d379cfc86d8aee092a7e1b662f26
zakiia <zakiasultanabhuiyan@gmail.com>
2024-07-29 13:07:45 +0600

    just finished

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,11 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2>this is about</h2>
+      <h2 className="services-title mt-5">Welcome To About Page</h2>
+      <p className=" text-center mt-5">
+        I'm Zakia Sultana Bhuiyan and i'm so shy to tell anything about myself,
+        so let's just skip this part.{" "}
+      </p>
     </div>
   );
 };
