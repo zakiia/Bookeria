4cc9ccb238163934ff0a08d5f7fa18e655d54a23
zakiia <zakiasultanabhuiyan@gmail.com>
2024-07-29 17:25:57 +0600

    adding website title

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,8 +5,8 @@ const About = () => {
     <div>
       <h2 className="services-title mt-5">Welcome To About Page</h2>
       <p className=" text-center mt-5">
-        I'm Zakia Sultana Bhuiyan and i'm so shy to tell anything about myself,
-        so let's just skip this part.{" "}
+        Hi, I'm Zakia Sultana Bhuiyan and i'm so shy to tell anything about
+        myself, so let's just skip this part.{" "}
       </p>
     </div>
   );
